6ed1eac2e9be677d9a27591be09d19b77d916640
Poorvaj-HV <poorvajvishwanath19@gmail.com>
2025-10-14 23:59:33 +0530

    review author for reviews

--- routes/review.js
+++ routes/review.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true }); // to access the params from the parent router(app.js file) like :id of listings, else it will be undefined and error will occur after submitting the review form
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body); // validating the request body against the reviewSchema defined in schema.js file
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(","); // joining all the error messages into a single string 
-        throw new ExpressError(400, error); // if there is an error in validation, throw an error with status code 400 and the error message
-    } else {
-        next();
-    }
-}
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 //Reviews
 //Post route 
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id; // setting the author of the review to the currently logged in user
 
     listing.reviews.push(newReview);
 
@@ -32,7 +22,7 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
 }));
 
 //Delete route
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
     let {id, reviewId} = req.params;
 
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}); // pulling(delete/update) out the reviewId from the reviews array in the listing document
